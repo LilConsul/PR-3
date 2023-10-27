660a3b1d0f638f42757dfcc9a1cdb19c68682431
Denis Shevchenko <denshevchenko19@gmail.com>
2023-10-27 20:25:13 +0200

    Front UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -24,11 +24,304 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.Task1_panel = new System.Windows.Forms.Panel();
+            this.ans1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.calc1 = new System.Windows.Forms.Button();
+            this.c_box1 = new System.Windows.Forms.TextBox();
+            this.b_box1 = new System.Windows.Forms.TextBox();
+            this.a_box1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Task2_panel = new System.Windows.Forms.Panel();
+            this.ans2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.calc2 = new System.Windows.Forms.Button();
+            this.b_box2 = new System.Windows.Forms.TextBox();
+            this.a_box2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.task1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Task1_panel.SuspendLayout();
+            this.Task2_panel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Task1_panel
+            // 
+            this.Task1_panel.Controls.Add(this.ans1);
+            this.Task1_panel.Controls.Add(this.label5);
+            this.Task1_panel.Controls.Add(this.calc1);
+            this.Task1_panel.Controls.Add(this.c_box1);
+            this.Task1_panel.Controls.Add(this.b_box1);
+            this.Task1_panel.Controls.Add(this.a_box1);
+            this.Task1_panel.Controls.Add(this.label4);
+            this.Task1_panel.Controls.Add(this.label3);
+            this.Task1_panel.Controls.Add(this.label2);
+            this.Task1_panel.Controls.Add(this.label1);
+            this.Task1_panel.Location = new System.Drawing.Point(0, 27);
+            this.Task1_panel.Name = "Task1_panel";
+            this.Task1_panel.Size = new System.Drawing.Size(426, 422);
+            this.Task1_panel.TabIndex = 6;
+            // 
+            // ans1
+            // 
+            this.ans1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ans1.Location = new System.Drawing.Point(139, 309);
+            this.ans1.Name = "ans1";
+            this.ans1.Size = new System.Drawing.Size(121, 42);
+            this.ans1.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 309);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 42);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Відповідь";
+            // 
+            // calc1
+            // 
+            this.calc1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calc1.Location = new System.Drawing.Point(85, 354);
+            this.calc1.Name = "calc1";
+            this.calc1.Size = new System.Drawing.Size(279, 59);
+            this.calc1.TabIndex = 13;
+            this.calc1.Text = "Розрахувати";
+            this.calc1.UseVisualStyleBackColor = true;
+            // 
+            // c_box1
+            // 
+            this.c_box1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.c_box1.Location = new System.Drawing.Point(85, 248);
+            this.c_box1.Name = "c_box1";
+            this.c_box1.Size = new System.Drawing.Size(98, 31);
+            this.c_box1.TabIndex = 10;
+            this.c_box1.Text = "5";
+            // 
+            // b_box1
+            // 
+            this.b_box1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_box1.Location = new System.Drawing.Point(85, 195);
+            this.b_box1.Name = "b_box1";
+            this.b_box1.Size = new System.Drawing.Size(98, 31);
+            this.b_box1.TabIndex = 11;
+            this.b_box1.Text = "4";
+            // 
+            // a_box1
+            // 
+            this.a_box1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a_box1.Location = new System.Drawing.Point(85, 151);
+            this.a_box1.Name = "a_box1";
+            this.a_box1.Size = new System.Drawing.Size(98, 31);
+            this.a_box1.TabIndex = 12;
+            this.a_box1.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 248);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "c = ";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "b =";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 31);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "a = ";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(402, 114);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Задано три числа. Якщо хоч одне з них менше 15, то знайти середнє арифметичне всі" + "х. В іншому випадку – суму.";
+            // 
+            // Task2_panel
+            // 
+            this.Task2_panel.Controls.Add(this.ans2);
+            this.Task2_panel.Controls.Add(this.label7);
+            this.Task2_panel.Controls.Add(this.calc2);
+            this.Task2_panel.Controls.Add(this.b_box2);
+            this.Task2_panel.Controls.Add(this.a_box2);
+            this.Task2_panel.Controls.Add(this.label9);
+            this.Task2_panel.Controls.Add(this.label10);
+            this.Task2_panel.Controls.Add(this.label11);
+            this.Task2_panel.Location = new System.Drawing.Point(432, 27);
+            this.Task2_panel.Name = "Task2_panel";
+            this.Task2_panel.Size = new System.Drawing.Size(426, 422);
+            this.Task2_panel.TabIndex = 16;
+            // 
+            // ans2
+            // 
+            this.ans2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ans2.Location = new System.Drawing.Point(139, 309);
+            this.ans2.Name = "ans2";
+            this.ans2.Size = new System.Drawing.Size(121, 42);
+            this.ans2.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(12, 309);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 42);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Відповідь";
+            // 
+            // calc2
+            // 
+            this.calc2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.calc2.Location = new System.Drawing.Point(85, 354);
+            this.calc2.Name = "calc2";
+            this.calc2.Size = new System.Drawing.Size(279, 59);
+            this.calc2.TabIndex = 13;
+            this.calc2.Text = "Розрахувати";
+            this.calc2.UseVisualStyleBackColor = true;
+            // 
+            // b_box2
+            // 
+            this.b_box2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b_box2.Location = new System.Drawing.Point(85, 195);
+            this.b_box2.Name = "b_box2";
+            this.b_box2.Size = new System.Drawing.Size(98, 31);
+            this.b_box2.TabIndex = 11;
+            this.b_box2.Text = "3";
+            // 
+            // a_box2
+            // 
+            this.a_box2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a_box2.Location = new System.Drawing.Point(85, 151);
+            this.a_box2.Name = "a_box2";
+            this.a_box2.Size = new System.Drawing.Size(98, 31);
+            this.a_box2.TabIndex = 12;
+            this.a_box2.Text = "2";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(12, 195);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 31);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "b =";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(12, 151);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 31);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "a =";
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(12, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(402, 114);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "На заданому проміжку чисел [a,b] підрахуйте добуток цілих чисел парних і кратних " + "7.";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.task1ToolStripMenuItem, this.task2ToolStripMenuItem });
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(902, 24);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // task1ToolStripMenuItem
+            // 
+            this.task1ToolStripMenuItem.Name = "task1ToolStripMenuItem";
+            this.task1ToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.task1ToolStripMenuItem.Text = "Task 1";
+            this.task1ToolStripMenuItem.Click += new System.EventHandler(this.task1_ToolStripMenuItem_Click);
+            // 
+            // task2ToolStripMenuItem
+            // 
+            this.task2ToolStripMenuItem.Name = "task2ToolStripMenuItem";
+            this.task2ToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.task2ToolStripMenuItem.Text = "Task 2";
+            this.task2ToolStripMenuItem.Click += new System.EventHandler(this.task2ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(902, 760);
+            this.Controls.Add(this.Task2_panel);
+            this.Controls.Add(this.Task1_panel);
+            this.Controls.Add(this.menuStrip1);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Task1_panel.ResumeLayout(false);
+            this.Task1_panel.PerformLayout();
+            this.Task2_panel.ResumeLayout(false);
+            this.Task2_panel.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.ToolStripMenuItem task1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task2ToolStripMenuItem;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        
+        private System.Windows.Forms.Label ans1;
+        private System.Windows.Forms.Label ans2;
+        
+        private System.Windows.Forms.Panel Task1_panel;
+        private System.Windows.Forms.Panel Task2_panel;
+        
+        private System.Windows.Forms.MenuStrip menuStrip1;
+
+        private System.Windows.Forms.Button calc1;
+        private System.Windows.Forms.Button calc2;
+
+        private System.Windows.Forms.TextBox a_box1;
+        private System.Windows.Forms.TextBox b_box1;
+        private System.Windows.Forms.TextBox c_box1;
+        private System.Windows.Forms.TextBox a_box2;
+        private System.Windows.Forms.TextBox b_box2;
+        
+
 
         #endregion
     }
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,18 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace PR_3 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        private void task1_ToolStripMenuItem_Click(object sender, EventArgs e) {
+            Task1_panel.Height += 1;
+        }
+
+        private void task2ToolStripMenuItem_Click(object sender, EventArgs e) {
+            Task2_panel.Height += 1;
         }
     }
 }
